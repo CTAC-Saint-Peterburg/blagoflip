facec6236fa82ac904bbaec4b9d385de89114f93
Стас <seleznev-00@mail.ru>
2023-01-14 13:39:31 +0300

    init: main page

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -2,8 +2,8 @@ import styles from "../styleComponents/Balance.module.css";
 export default function Balance() {
   return (
     <div className={styles.main}>
-      <h3>Balance</h3>
-      <h2>10.00$</h2>
+      <h3 className={styles.h3Balance}>Balance</h3>
+      <h2 className={styles.h2Balance}>10.00$</h2>
       <div className={styles.buttonblock}>
         <button>снять</button>
         <button>пополнить</button>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,4 +1,14 @@
 import styles from "../styleComponents/Profile.module.css";
+import img from "../images/avatar.png";
 export default function Profile() {
-  return <div className={styles.main}>Profile</div>;
+  return (
+    <div className={styles.main}>
+      <h3 className={styles.h3Profile}>ID#1</h3>
+      <div className={styles.imgwrapper}>
+        <img className={styles.imgProfile} src={img} alt="Avatar" />
+        <h2 className={styles.h2Profile}>crazyBet</h2>
+      </div>
+      <h4 className={styles.h4Winrate}>Winrate 51%</h4>
+    </div>
+  );
 }
