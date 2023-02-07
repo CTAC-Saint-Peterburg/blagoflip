4ca04c738b9f9720b453be24849eee509d68a1fe
Стас <seleznev-00@mail.ru>
2023-02-07 21:50:40 +0300

    update: Chat component

--- src/components/componentsChat/ChatframeHeader.jsx
+++ src/components/componentsChat/ChatframeHeader.jsx
@@ -1,4 +1,10 @@
 import styles from "./styleComponent/ChatframeHeader.module.css";
 export default function ChatframeHeader() {
-  return <div className={styles.main}>Header</div>;
+  return (
+    <div className={styles.main}>
+      <h2 className={styles.header2}>Name</h2>
+      <span className={styles.spanData}>Online</span>
+      <span className={styles.spanData}>last seen 07.02.2023</span>
+    </div>
+  );
 }
